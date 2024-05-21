6d83de1ce28698b2f7e7df82402f4c25e28072a6
sin <birthdw@naver.com>
2024-05-21 14:06:22 +0900

    Revert "AWS"
    This reverts commit 86ef6d4745540d8fad4e4434af80e45d6a329f3e.

--- MainTool/AWS.h
+++ MainTool/AWS.h
@@ -10,8 +10,6 @@
 #include <aws/s3/model/PutObjectRequest.h>
 #include <aws/s3/model/DeleteObjectsRequest.h>
 
-#include "RobotInfo.h"
-
 using namespace std;
 using namespace Aws;
 using namespace Aws::Client;
